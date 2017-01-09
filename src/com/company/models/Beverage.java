df398cf63157121b27a8eff22d075aad86f4b8ea
danielHPeters <daniel.peters.ch@gmail.com>
2017-01-09 15:06:51 +0100

    Implemented the Shop and Beverage Classes

--- src/com/company/models/Beverage.java
+++ src/com/company/models/Beverage.java
@@ -3,5 +3,29 @@ package com.company.models;
 /**
  * Created by Daniel on 09.01.2017.
  */
-public class Beverage {
+public class Beverage extends Item implements Consumable{
+
+    /**
+     *
+     */
+    private int volume;
+
+    /**
+     *
+     * @param price
+     * @param name
+     * @param details
+     * @param priceDrop
+     */
+    public Beverage(double price, String name, String details, int priceDrop){
+        super(price, name, details, priceDrop);
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void consume() {
+
+    }
 }
--- src/com/company/models/Shop.java
+++ src/com/company/models/Shop.java
@@ -1,7 +1,66 @@
 package com.company.models;
 
+import com.company.models.people.Employee;
+
+import java.util.List;
+
 /**
  * Created by Daniel on 09.01.2017.
  */
 public class Shop {
+
+    /**
+     *
+     */
+    private List<Item> stock;
+
+    /**
+     *
+     */
+    private List<Consumable> consumableStock;
+
+    /**
+     *
+     */
+    private Employee employee;
+
+    /**
+     *
+     * @return
+     */
+    public List<Item> getStock() {
+        return stock;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<Consumable> getConsumableStock() {
+        return consumableStock;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    /**
+     *
+     * @param stock
+     */
+    public void setStock(List<Item> stock) {
+        this.stock = stock;
+    }
+
+    /**
+     *
+     * @param consumableStock
+     */
+    public void setConsumableStock(List<Consumable> consumableStock) {
+        this.consumableStock = consumableStock;
+    }
 }
